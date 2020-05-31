ca2d5fa25c154dd04cd274441bff00f845469ff5
Kiran Deshmukh <kiran.deshmukh.vc@startv.com>
2020-05-31 16:57:35 +0530

    [cors] Enable cors.

--- src/app/main/__init__.py
+++ src/app/main/__init__.py
@@ -4,12 +4,15 @@ from flask_bcrypt import Bcrypt
 
 from .config import config_by_name
 
+from flask_cors import CORS
+
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
